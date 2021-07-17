7284f163d3689f579c00c0b6ffde506f91214882
Yean Seyha <yeanseyha7@gmail.com>
2021-07-17 08:46:36 +0700

    Laravel 8 From Scratch

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quos dicta eaque consequatur odio dolore deleniti amet labore excepturi numquam voluptatem nulla, tempore, dolorum nam corrupti unde. In, rem nisi?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quos dicta eaque consequatur odio dolore deleniti amet labore excepturi numquam voluptatem nulla, tempore, dolorum nam corrupti unde. In, rem nisi?</p>
+    </article>
+
+   
+
+    
+</body>
